21a453d527513d3c7d6cf54840e6a37b6d0bf712
Maximilian Wittig <maximilian.wittig@ur.de>
2022-04-19 22:19:53 +0200

    Only limit homework

--- praktomat_limit_submissions.py
+++ praktomat_limit_submissions.py
@@ -6,37 +6,43 @@ import os
 
 
 def limit_submissions(conn, task, max_upload):
-    query = "UPDATE solutions_solution SET accepted = 'f', final = 'f' WHERE number > {} AND task_id = {};".format(
+    query_above = "UPDATE solutions_solution SET accepted = 'f', final = 'f' WHERE number > {} AND task_id = {};".format(
         max_upload, task
     )
-    print(query)
-    try:
-        cursor = conn.cursor()
-        cursor.execute(query)
-        cursor.close()
 
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-
-def get_tasks(conn, regex_task):
-    tasks = []
-    query_get_tasks = (
-        "SELECT id FROM tasks_task WHERE title SIMILAR TO '{}' ORDER BY id ASC;".format(
-            regex_task
-        )
+    query_max = "UPDATE solutions_solution SET accepted = 't', final = 't' WHERE number = {} AND task_id = {};".format(
+        max_upload, task
     )
+    print(query_above)
+    print(query_max)
+    run_sql(conn, query_above)
+    run_sql(conn, query_max)
+
+
+def get_tasks(conn, regex_task, rating_scale):
+    query_get_tasks = "SELECT id FROM tasks_task WHERE title SIMILAR TO '{}' AND final_grade_rating_scale_id = {} ORDER BY id ASC;".format(
+        regex_task, rating_scale
+    )
+    print(query_get_tasks)
+    return run_sql(conn, query_get_tasks)
+
+
+def get_rating(conn, rating_name):
+    query_get_rating = "SELECT id FROM attestation_ratingscale WHERE name = '{}' ORDER BY id ASC;".format(
+        rating_name
+    )
+    print(query_get_rating)
+    return run_sql(conn, query_get_rating)[0]
+
+
+def run_sql(conn, sql):
     try:
         cursor = conn.cursor()
-        cursor.execute(query_get_tasks)
-        task_id = cursor.fetchone()
-
-        while task_id is not None:
-            tasks += task_id
-            task_id = cursor.fetchone()
+        cursor.execute(sql)
+        results = [record for record in cursor]
 
         cursor.close()
-        return tasks
+        return results
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
 
@@ -44,7 +50,13 @@ def get_tasks(conn, regex_task):
 def connect_db():
     try:
         ps_conn = psycopg2.connect(
-            "host=DB_HOST port=DB_PORT dbname=DB_NAME user=DB_USER password=DB_PASS"
+            "host={} port={} dbname={} user={} password={}".format(
+                os.environ["DB_HOST"],
+                os.environ["DB_PORT"],
+                os.environ["DB_NAME"],
+                os.environ["DB_USER"],
+                os.environ["DB_PASS"],
+            )
         )
         ps_conn.autocommit = True
         return ps_conn
@@ -56,7 +68,8 @@ max_uploads = (
     os.environ["PRAKTOMAT_MAX_UPLOADS"] if "PRAKTOMAT_MAX_UPLOADS" in os.environ else 3
 )
 conn = connect_db()
-tasks = get_tasks(conn, "(OOP|ADP): H[0-9]{2}%")
+rating_scale = get_rating(conn, "SBL")
+tasks = get_tasks(conn, "(OOP|ADP): H[0-9]{2}%", rating_scale)
 for task in tasks:
     limit_submissions(conn, task, max_uploads)
 conn.close()
